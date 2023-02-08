d09c3d6eb2fe455db764d5c172fff54e8eba6abf
Suellen.Dev <87045491+suellencosta7@users.noreply.github.com>
2023-02-07 21:49:08 -0300

    Update Comandos.sql

--- Comandos.sql
+++ Comandos.sql
@@ -8,4 +8,7 @@ desc aluno;
 
 insert into aluno value (default,'Samira Andrade Silva','2009-09-29','1326244539');
 
-insert into aluno values (default, 'Ramiro Alves Fitman','1994-05-30',null),(default, 'Yiara Martins Santos','2006-03-10','78624598');
+# null para passar o valor da tabela zerado
+
+insert into aluno values (default, 'Ramiro Alves Fitman','1994-05-30',null),(default, 'Yiara Martins Santos','2006-03-10','78624598'); 
+update aluno set cpf='6429456' where id ='3';             -- Inserir dado em determinado campo
